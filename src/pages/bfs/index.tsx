fcf912bd3d1b20a16ae73dca8aa19fa180b857e9
snowyuki <y-maeda.1031@keio.jp>
2022-08-23 02:43:50 +0900

    initial commit

---
+++ src/pages/bfs/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const BFS: NextPage = () => {
+  return <></>;
+};
+
+export default BFS;
---
+++ src/pages/calc/index.tsx
@@ -0,0 +1,41 @@
+import type { NextPage } from "next";
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
+import init, { add } from "wasm-lib";
+
+const Calculator: NextPage = () => {
+  const [val1, setVal1] = useState(0);
+  const [val2, setVal2] = useState(0);
+  const [ans, setAns] = useState(0);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    f: Dispatch<SetStateAction<number>>
+  ) => {
+    f(() => Number(e.target.value));
+  };
+
+  useEffect(() => {
+    init().then(() => {
+      setAns(add(val1, val2));
+    });
+  }, [val1, val2]);
+
+  return (
+    <>
+      <input
+        type="number"
+        value={val1}
+        onChange={(e) => handleChange(e, setVal1)}
+      />{" "}
+      +{" "}
+      <input
+        type="number"
+        value={val2}
+        onChange={(e) => handleChange(e, setVal2)}
+      />{" "}
+      = {ans}
+    </>
+  );
+};
+
+export default Calculator;
