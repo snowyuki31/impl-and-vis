7e3bea334b822e91d2fd032dd424b0d272662864
snowyuki <y-maeda.1031@keio.jp>
2022-09-02 09:18:02 +0900

    add p5js

---
+++ src/components/blocks/sketch/index.tsx
@@ -0,0 +1,19 @@
+import dynamic from "next/dynamic";
+import p5Types from "p5";
+
+const Sketch = dynamic(import("react-p5"), {
+  loading: () => <></>,
+  ssr: false,
+});
+
+export const SketchComponent = () => {
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+    p5.colorMode(p5.HSB, p5.width, p5.height, 100);
+    p5.noStroke();
+  };
+
+  return <></>;
+};
+
+export default SketchComponent;
